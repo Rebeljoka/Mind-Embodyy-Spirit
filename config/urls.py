05735c4482710c38ea70096a38b354bb83769c2d
Rebeljoka <habib.u99@googlemail.com>
2025-10-14 22:31:29 +0100

    Add: Core app for homepage, updated base.html to exlusively only display full width, updated urls to include home, added core app to settings, home renamed to index

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     # Root landing page
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path(
+        '',
+        TemplateView.as_view(template_name='core/index.html'),
+        name='home',
+    ),
 ]
 
 if settings.DEBUG:
