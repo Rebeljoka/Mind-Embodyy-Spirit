42470f03df0d297e0450ef47b8f28a8f947ee432
RocioSerrano0921 <rocioserranogomez21@gmail.com>
2025-10-15 17:50:57 +0100

    Add gallery app models, admin, and initial migration

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Artist, Category, Painting, PaintingImage
 
 # Register your models here.
+
+admin.site.register(Artist)
+admin.site.register(Category)
+admin.site.register(Painting)
+admin.site.register(PaintingImage)
