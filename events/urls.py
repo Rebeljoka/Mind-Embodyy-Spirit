8863da8d626552458590303faea32360f5ae19b5
PeterCones <olly.john@live.co.uk>
2025-10-17 14:27:15 +0000

    add 'add event' view/button to events. Restore previous changes to base.html

--- events/urls.py
+++ events/urls.py
@@ -6,6 +6,7 @@ app_name = 'events'
 
 urlpatterns = [
     path('', views.event_list_view, name='events'),
+    path('new/', views.newEvent, name='event_new'),
         path('<int:pk>/edit/', views.event_edit_view, name='event_edit'),
         path('<int:pk>/delete/', views.event_delete, name='event_delete'),
 ]
